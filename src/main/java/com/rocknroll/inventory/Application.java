2510bb936cef396138c07f0a249fa4afdfed0e57
mfedechko <mfedechko@gmail.com>
2025-10-28 10:20:03 +0200

    added pim.xml and application files

---
+++ src/main/java/com/rocknroll/inventory/Application.java
@@ -0,0 +1,15 @@
+package com.rocknroll.inventory;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mfedechko
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/rocknroll/inventory/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.rocknroll.inventory.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author mfedechko
+ */
+@Configuration
+public class AppConfig {
+}
